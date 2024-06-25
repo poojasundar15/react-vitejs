ad258a6065e2057f77fd1e1fce96459204a51cfc
poojasundar99 <poojasundar519@gmail.com>
2024-06-25 23:32:54 +0530

    first commit

---
+++ class1/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { LearnComponents } from './Components/LearnComponents'
+
+function App() {
+
+  return (
+    <div>
+      <div>This is text from pooja</div>
+      <LearnComponents/>
+      </div>
+  )
+}
+
+export default App
---
+++ class1/src/Components/LearnComponents.jsx
@@ -0,0 +1,8 @@
+export const LearnComponents = () => {
+  return (
+    <div>
+      This is a text form pooja LearnComponents jsx
+    </div>
+  )
+};
+
